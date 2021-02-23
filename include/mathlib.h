02dcf7bd0cfcfbc6de279ff7b5e80b2090a21355
Charathram Ranganathan <charath@pfactor.io>
2021-02-22 16:06:43 -0800

    Initial Commit

---
+++ include/mathlib.h
@@ -0,0 +1,6 @@
+#ifndef MATHLIB
+#define MATHLIB
+
+int multiply_by(int operand, int factor);
+
+#endif
---
+++ mathlib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int multiply_by(int operand, int factor) {
+	return operand * factor;
+}
+
